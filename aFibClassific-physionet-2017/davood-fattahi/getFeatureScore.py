c70a3af12e669ca0558009f0a3c4086d14f5505f
Davood_Fattahi <fattahi.d@gmail.com>
2022-04-11 17:34:19 +0430

    daily update

--- aFibClassific-physionet-2017/davood-fattahi/getFeatureScore.py
+++ aFibClassific-physionet-2017/davood-fattahi/getFeatureScore.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.feature_selection import mutual_info_classif, f_classif, chi2, SelectKBest, VarianceThreshold
 from ncafs import NCAFSC
 # import pymrmr
-# import numpy as np
+import numpy as np
 import time
 
 
@@ -16,16 +16,16 @@ def getFeatureScore(features, labels, methods=[]):
     # replace any nan with zero
     features = features.fillna(0)
 
-    # normalizing the data
-    features[:] = preprocessing.MinMaxScaler(
-    ).fit_transform(features)
+    # # scaling and standardizing the data
+    features_sc = preprocessing.MinMaxScaler().fit_transform(features)
+    # features_st = preprocessing.StandardScaler().fit_transform(features)
 
     scores = {}
     if (not(bool(methods))) | ('var_threshold' in methods):
         # # 1-1- variance thresholding
         s = time.time()
         vthSelector = VarianceThreshold()
-        vthSelector.fit(features, labels)
+        vthSelector.fit(features_sc, labels)
         scores['var_threshold'] = vthSelector.variances_
         print("var_threshold running time:", time.time()-s)
 
@@ -33,7 +33,7 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-2- chi-squared test
         s = time.time()
         chi2Selector = SelectKBest(
-            chi2).fit(features, labels)
+            chi2).fit(features_sc, labels)
         scores['chi2_test'] = chi2Selector.scores_
         print("chi2_test running time:", time.time()-s)
 
@@ -41,8 +41,8 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-3- ANOVA F-value
         s = time.time()
         fvSelector = SelectKBest(f_classif).fit(
-            features, labels)
-        # fvaluelFeatures_ind = fvSelector.get_support(indices=True)
+            features_sc, labels)
+        # fvaluelfeatures_sc_ind = fvSelector.get_support(indices=True)
         scores['f_value'] = fvSelector.scores_
         print("f_value running time:", time.time()-s)
 
@@ -50,26 +50,26 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-4-  mutual_info
         s = time.time()
         mutInfSelector = SelectKBest(
-            mutual_info_classif).fit(features, labels)
+            mutual_info_classif).fit(features_sc, labels)
         scores['mutual_info'] = mutInfSelector.scores_
         print("mutual_info running time:", time.time()-s)
 
     # if (not bool(methods)) | ('mrmr' in methods):
     #     # # (skipped) 1-5- MRMR
     #     s = time.time()
-    #     df = features.copy()
+    #     df = features_sc.copy()
     #     df.insert(0, "labels", labels)
-    #     mrmrSelector = pymrmr.mRMR(df, 'MIQ', features.shape[1])
-    #     # mrmrSelector = pymrmr.mRMR(df, 'MID', features.shape[1])
-    #     scores['mrmr'] = features.shape[1] - \
-    #         np.array([mrmrSelector.index(i) for i in features.columns])
+    #     mrmrSelector = pymrmr.mRMR(df, 'MIQ', features_sc.shape[1])
+    #     # mrmrSelector = pymrmr.mRMR(df, 'MID', features_sc.shape[1])
+    #     scores['mrmr'] = features_sc.shape[1] - \
+    #         np.array([mrmrSelector.index(i) for i in features_sc.columns])
     #     print("mrmr running time:", time.time()-s)
 
     if (not bool(methods)) | ('nca' in methods):
         # # 1-6- NCAFSC (heavy comp cost)
         s = time.time()
         ncaSelector = NCAFSC()
-        ncaSelector.fit(features, labels)
+        ncaSelector.fit(features_sc, labels)
         scores['nca'] = ncaSelector.weights_
         print("nca running time:", time.time()-s)
 
@@ -77,7 +77,7 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-7- ReliefF (very heavy comp cost)
         s = time.time()
         reliffSelector = ReliefF()
-        reliffSelector.fit(features.to_numpy(), labels.to_numpy())
+        reliffSelector.fit(features_sc.to_numpy(), labels.to_numpy())
         scores['relieff'] = reliffSelector.feature_importances_
         print("relieff running time:", time.time()-s)
 
@@ -85,7 +85,7 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-8- SURF (very heavy comp cost)
         s = time.time()
         surfSelector = SURF()
-        surfSelector.fit(features.to_numpy(), labels.to_numpy())
+        surfSelector.fit(features_sc.to_numpy(), labels.to_numpy())
         scores['surf'] = surfSelector.feature_importances_
         print("surf running time:", time.time()-s)
 
@@ -93,7 +93,7 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-9- SURFstar (very heavy comp cost)
         s = time.time()
         surfstarSelector = SURFstar()
-        surfstarSelector.fit(features.to_numpy(), labels.to_numpy())
+        surfstarSelector.fit(features_sc.to_numpy(), labels.to_numpy())
         scores['surf*'] = surfstarSelector.feature_importances_
         print("surf* running time:", time.time()-s)
 
@@ -101,7 +101,7 @@ def getFeatureScore(features, labels, methods=[]):
        # # 1-10- MultiSURF (very heavy comp cost)
         s = time.time()
         multisurfSelector = MultiSURF()
-        multisurfSelector.fit(features.to_numpy(), labels.to_numpy())
+        multisurfSelector.fit(features_sc.to_numpy(), labels.to_numpy())
         scores['multisurf'] = multisurfSelector.feature_importances_
         print("multisurf running time:", time.time()-s)
 
@@ -109,7 +109,7 @@ def getFeatureScore(features, labels, methods=[]):
         # # 1-11- MultiSURFstar (heavy comp cost)
         s = time.time()
         multisurfstarSelector = MultiSURFstar()
-        multisurfstarSelector.fit(features.to_numpy(), labels.to_numpy())
+        multisurfstarSelector.fit(features_sc.to_numpy(), labels.to_numpy())
         scores['multisurf*'] = multisurfstarSelector.feature_importances_
         print("multisurf* running time:", time.time()-s)
 
@@ -117,16 +117,36 @@ def getFeatureScore(features, labels, methods=[]):
         # 1-12- turf (very heavy comp cost)
         # turf is a iterative approach of relieff algorithm, suitable for high dimensional feature space (>10000)
         s = time.time()
-        headers = list(features)
+        headers = list(features_sc)
         turfSelector = TuRF(core_algorithm="ReliefF", verbose=True)
-        turfSelector.fit(features.to_numpy(),
+        turfSelector.fit(features_sc.to_numpy(),
                          labels.to_numpy(), headers)
         scores['turf'] = turfSelector.feature_importances_
         print("turf running time:", time.time()-s)
 
     # # (skipped) 1-12- Laplacian score
-    # a = skfeature.function.similarity_based.lap_score(features)
+    # a = skfeature.function.similarity_based.lap_score(features_sc)
 
-    featureScores = pd.DataFrame(data=scores)
+    featureScores = pd.DataFrame(data=scores, index=features.columns)
 
     return featureScores
+
+
+def selectByVote(df, n):
+    a = []
+    for i in df.columns:
+        a = a+list(df.sort_values(
+            by=i, ascending=False).index[0:n])
+    values, counts = np.unique(a, return_counts=True)
+    f = values[(-counts).argsort()][0:n]
+    return f
+
+
+# def selectByWeight(df, n):
+#     a = []
+#     for i in df.columns:
+#         a = a+list(df.sort_values(
+#             by=i, ascending=False).index[0:n])
+#     values, counts = np.unique(a, return_counts=True)
+#     f = values[(-counts).argsort()][0:n]
+#     return f
--- aFibClassific-physionet-2017/davood-fattahi/selectFeatures.py
+++ aFibClassific-physionet-2017/davood-fattahi/selectFeatures.py
@@ -1,20 +1,23 @@
 
 # import scipy.io
 # import os
-# import tsfresh
 import numpy as np
 # import pickle
-import pymrmr
-from ncafs import NCAFSC
-from sklearn.feature_selection import mutual_info_classif, f_classif, chi2, SelectKBest, VarianceThreshold
 import pandas as pd
-from sklearn import preprocessing
-# import skfeature
-from skrebate import ReliefF, SURF, SURFstar, MultiSURF, MultiSURFstar, TuRF
-# import mifs
-from getFeatureScore import getFeatureScore
+from sklearn import preprocessing, model_selection
+from getFeatureScore import getFeatureScore, selectByVote
 
 
+# perpare the labels
+labels = pd.read_csv('REFERENCE-v3.csv', header=None)
+labels.columns = ["recordName", "className"]
+labels["classNum"] = np.nan
+labels.at[labels.className == 'N', 'classNum'] = 0
+labels.at[labels.className == 'A', 'classNum'] = 1
+labels.at[labels.className == 'O', 'classNum'] = 2
+labels.at[labels.className == '~', 'classNum'] = 3
+
+# load the features
 GenericFeatures = pd.read_csv('GenericFeatures.csv')
 SemiGenericFeatures = pd.read_csv('SemiGenericFeatures_meanBeat.csv')
 DataDerivenFeatures = pd.read_csv('DataDerivenFeatures.csv')
@@ -42,155 +45,61 @@ ModelBasedFeatures = ModelBasedFeatures.drop(
 # allFeatures = pd.concat([GenericFeatures, SemiGenericFeatures,
 #                         DataDerivenFeatures, ModelBasedFeatures], axis=1)
 
-# normalizing the data
-GenericFeatures[:] = preprocessing.MinMaxScaler(
-).fit_transform(GenericFeatures)
-SemiGenericFeatures[:] = preprocessing.MinMaxScaler(
-).fit_transform(SemiGenericFeatures)
-DataDerivenFeatures[:] = preprocessing.MinMaxScaler(
-).fit_transform(DataDerivenFeatures)
-ModelBasedFeatures[:] = preprocessing.MinMaxScaler(
-).fit_transform(ModelBasedFeatures)
-# allFeatures[:] = preprocessing.MinMaxScaler().fit_transform(allFeatures)
+
+# the feature-selection MUST be included in cross-validation! see: https://www.nodalpoint.com/not-perform-feature-selection/
+
+# train-validation split
+GF_train, GF_val, SGF_train, SGF_val, DDF_train, DDF_val, MBF_train, MBF_val, labels_train, labels_val = model_selection.train_test_split(
+    GenericFeatures, SemiGenericFeatures, DataDerivenFeatures, ModelBasedFeatures, labels, test_size=0.2, random_state=42)
 
 
-# perpare the labels
-labels = pd.read_csv('REFERENCE-v3.csv', header=None)
-labels.columns = ["recordName", "className"]
-labels["classNum"] = np.nan
-labels.at[labels.className == 'N', 'classNum'] = 0
-labels.at[labels.className == 'A', 'classNum'] = 1
-labels.at[labels.className == 'O', 'classNum'] = 2
-labels.at[labels.className == '~', 'classNum'] = 3
+# scaling the features (or standardization?)
+GF_scalar = preprocessing.MinMaxScaler().fit(GF_train)
+GF_train_sc = pd.DataFrame(GF_scalar.transform(
+    GF_train), columns=GF_train.columns)
+
+SGF_scalar = preprocessing.MinMaxScaler().fit(SGF_train)
+SGF_train_sc = pd.DataFrame(SGF_scalar.transform(
+    SGF_train), columns=SGF_train.columns)
+
+DDF_scalar = preprocessing.MinMaxScaler().fit(DDF_train)
+DDF_train_sc = pd.DataFrame(DDF_scalar.transform(
+    DDF_train), columns=DDF_train.columns)
+
+MBF_scalar = preprocessing.MinMaxScaler().fit(MBF_train)
+MBF_train_sc = pd.DataFrame(MBF_scalar.transform(
+    MBF_train), columns=MBF_train.columns)
 
 
 # initial selection
-GenericFeatures_scores = getFeatureScore(GenericFeatures, labels.classNum,
-                                         methods=['var_threshold', 'chi2_test', 'f_value', 'mutual_info'])
+methods = ['var_threshold', 'chi2_test', 'f_value', 'mutual_info']
+GFnames_best100 = selectByVote(getFeatureScore(
+    GF_train_sc, labels_train.classNum, methods=methods), 100)
+SGFnames_best100 = selectByVote(getFeatureScore(
+    SGF_train_sc, labels_train.classNum, methods=methods), 100)
+DDFnames_best100 = selectByVote(getFeatureScore(
+    DDF_train_sc, labels_train.classNum, methods=methods), 100)
+MBFnames_best100 = selectByVote(getFeatureScore(
+    MBF_train_sc, labels_train.classNum, methods=methods), 100)
 
 
-def selectByVote(df, n):
-    a = []
-    for i in df.columns:
-        a = a+list(GenericFeatures_scores.sort_values(
-            by=i, ascending=False).index[0:n])
-    values, counts = np.unique(a, return_counts=True)
-    f = values[(-counts).argsort()][0:n]
-    return f
+features_best400 = pd.concat(
+    [GF_train_sc[GFnames_best100], SGF_train_sc[SGFnames_best100],
+     DDF_train_sc[DDFnames_best100], MBF_train_sc[MBFnames_best100]], axis=1)
 
+methods = ['var_threshold', 'chi2_test', 'f_value', 'mutual_info', 'nca',
+           'relieff', 'surf', 'surf*', 'multisurf', 'multisurf*']
+fscores_best400 = getFeatureScore(
+    GF_train_sc, labels_train.classNum, methods=methods)
+allFeatures_best100 = selectByVote(fscores_best400, 100)
 
-f = selectByVote(GenericFeatures_scores, 100)
-
-
-a = GenericFeatures_scores.sort_values(
-    by='var_threshold', ascending=False).index[0:100]
-b = GenericFeatures_scores.sort_values(
-    by='chi2_test', ascending=False).index[0:100]
-c = GenericFeatures_scores.sort_values(
-    by='f_value', ascending=False).index[0:100]
-d = GenericFeatures_scores.sort_values(
-    by='mutual_info', ascending=False).index[0:100]
-
-e = list(a)+list(b)+list(c)+list(d)
-values, counts = np.unique(e, return_counts=True)
-f = values[(-counts).argsort()][0:100]
 # =============================================================================
 # Scenario 1: selecting among all the features
-
-
-# # 1-1- variance thresholding
-vthSelector = VarianceThreshold(threshold=.005)
-vthSelector.fit(allFeatures, labels)
-vthFeatures_ind = vthSelector.get_support(indices=True)
-vthFeatures = allFeatures.iloc[:, vthFeatures_ind]
-
-# # 1-2- chi-squared test
-chi2Selector = SelectKBest(
-    chi2, k=100).fit(vthFeatures, labels.className)
-chi2testlFeatures_ind = chi2Selector.get_support(indices=True)
-
-# # 1-3- ANOVA F-value
-fvSelector = SelectKBest(f_classif, k=100).fit(
-    vthFeatures, labels.className)
-fvaluelFeatures_ind = fvSelector.get_support(indices=True)
-# a = fvaluelFeatures.scores_
-# b = fvaluelFeatures.pvalues_
-
-# # 1-4-  mutual_info
-mutInfSelector = SelectKBest(
-    mutual_info_classif, k=100).fit(vthFeatures, labels.className)
-mutInfoFeatures_ind = mutInfSelector.get_support(indices=True)
-
-# # 1-5- MRMR
-df = vthFeatures.copy()
-df.insert(0, "labels", labels.classNum)
-mrmrSelector = pymrmr.mRMR(df, 'MIQ', 100)
-
-# # 1-6- NCAFSC
-ncaSelector = NCAFSC()
-ncaSelector.fit(vthFeatures, labels.classNum)
-w = ncaSelector.weights
-
-# # 1-7- ReliefF
-reliffSelector = ReliefF()
-reliffSelector.fit(vthFeatures.to_numpy(), labels.classNum.to_numpy())
-
-# # 1-8- SURF
-surfSelector = SURF()
-surfSelector.fit(vthFeatures.to_numpy(), labels.classNum.to_numpy())
-
-# # 1-9- SURFstar
-surfstarSelector = SURFstar()
-surfstarSelector.fit(vthFeatures.to_numpy(), labels.classNum.to_numpy())
-
-
-# # 1-10- MultiSURF
-multisurfSelector = MultiSURF()
-multisurfSelector.fit(vthFeatures.to_numpy(), labels.classNum.to_numpy())
-
-# # 1-11- MultiSURFstar
-multisurfstarSelector = MultiSURFstar()
-multisurfstarSelector.fit(vthFeatures.to_numpy(), labels.classNum.to_numpy())
-
-headers = list(vthFeatures)
-turfSelector = TuRF(core_algorithm="ReliefF",
-                    n_features_to_select=2, pct=0.5, verbose=True)
-turfSelector.fit(vthFeatures.to_numpy(), labels.classNum.to_numpy(), headers)
-
-turfSelector.feature_importances_
-
-# # (skipped) 1-12- Laplacian score
-# a = skfeature.function.similarity_based.lap_score(vthFeatures)
 
 # =============================================================================
 # =============================================================================
 # Scenario 2: selecting from each type and ranking the selected features
 
-# # 2-1- variance thresholding
-sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-DataDerivenFeatures_vth = sel.fit_transform(DataDerivenFeatures)
-
-# # 2-2- chi-squared test
-DataDerivenFeatures_chi2test = SelectKBest(
-    chi2, k=100).fit_transform(DataDerivenFeatures, labels.className)
-
-# # 2-3- ANOVA F-value
-DataDerivenFeatures_fvalue = SelectKBest(f_classif, k=100).fit_transform(
-    DataDerivenFeatures, labels.className)
-
-# # 2-4-  mutual_info
-DataDerivenFeatures_mutualInfo = SelectKBest(
-    mutual_info_classif, k=100).fit_transform(DataDerivenFeatures, labels.className)
-
-# # 2-5- NCAFSC
-fs_clf = NCAFSC()
-fs_clf.fit(DataDerivenFeatures, labels.className)
-w = fs_clf.weights
-
-# # 2-6- MRMR
-df = DataDerivenFeatures.copy()
-df.insert(0, "labels", labels.className)
-pymrmr.mRMR(df, 'MIQ', 100)
 # =============================================================================
 # =============================================================================
 # # Scenario 3: Classification results
